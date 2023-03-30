8be188d73ad471aca5a381a4ce076149b4a04521
Sergio Jimenez <serjicho@hotmail.com>
2023-03-30 03:10:06 +0200

    creates the experience component as a base point

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import {Canvas} from "@react-three/fiber";
+import Experience from "./components/Experience.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-    <>
-        <Canvas>
-            <mesh>
-                <torusKnotGeometry/>
-                <meshNormalMaterial/>
-            </mesh>
-        </Canvas>
-    </>
+    <Canvas>
+        <Experience/>
+    </Canvas>
 )
