fded781ba34a02bfc562e7fdb8b0ad607cc49672
Sergio Jimenez <serjicho@hotmail.com>
2023-03-31 15:41:41 +0200

    adds leva outside the canvas

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import {Canvas} from "@react-three/fiber";
 import Experience from "./experience/Experience.jsx";
+import {Leva} from "leva";
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
     <StrictMode>
+        <Leva/>
         <Canvas>
             <Experience/>
         </Canvas>
